518ba132c92cac70d8d9aa7f0e1ff70bb29a2a94
smfils1 <smfils1@gmail.com>
2020-10-16 23:39:48 -0400

    first commit

---
+++ src/main/java/com/spring/demo/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.spring.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+	@GetMapping
+	static public String hello() {
+		return "hello admin";
+	}
+}
