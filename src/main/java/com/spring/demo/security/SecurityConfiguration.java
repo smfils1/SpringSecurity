750ad8c095912ccc15826f12ee01ed8ecd9fd067
smfils1 <smfils1@gmail.com>
2020-10-18 02:59:18 -0400

    Added jwt config

--- src/main/java/com/spring/demo/security/SecurityConfiguration.java
+++ src/main/java/com/spring/demo/security/SecurityConfiguration.java
@@ -8,48 +8,54 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.spring.demo.repository.UserRepository;
+import com.spring.demo.security.filter.JwtAuthenticationFilter;
+import com.spring.demo.security.filter.JwtAuthorizationFilter;
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UserDetailsService userDetailService;
+	private UserPrincipalService userPrincipalService;
+	private UserRepository userRepository;
+
+	public SecurityConfiguration(UserPrincipalService userPrincipalService, UserRepository userRepository) {
+		this.userPrincipalService = userPrincipalService;
+		this.userRepository = userRepository;
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(authenticationProvider());
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.mvcMatchers("/").permitAll()
-        .mvcMatchers("/h2-console/**").permitAll()
-		.mvcMatchers("/customer").hasRole("CUSTOMER")
-		.mvcMatchers("/customer/vip").hasAnyAuthority("ACCESS_DISCOUNT", "ROLE_CUSTOMER")
-		.mvcMatchers("/admin").hasRole("ADMIN")
-		.and()
-		.csrf().disable()
-        .headers().frameOptions().sameOrigin() //For h2 console
-        .and()    
-		.httpBasic();
-		
-	}
-	
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+		http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().headers()
+				.frameOptions().sameOrigin().and().addFilter(new JwtAuthenticationFilter(authenticationManager()))
+				.addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository)).authorizeRequests()
+				.mvcMatchers("/").permitAll().mvcMatchers("/h2-console/**").permitAll().mvcMatchers("/login").permitAll().mvcMatchers("/customer")
+				.hasRole("CUSTOMER").mvcMatchers("/customer/vip").hasAnyAuthority("ACCESS_DISCOUNT", "ROLE_CUSTOMER")
+				.mvcMatchers("/admin").hasRole("ADMIN");
+				
 
-    
-    @Bean
-    DaoAuthenticationProvider authenticationProvider() {
-    	DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-    	daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-    	daoAuthenticationProvider.setUserDetailsService(userDetailService);
-    	return daoAuthenticationProvider;
-    }
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		daoAuthenticationProvider.setUserDetailsService(userPrincipalService);
+		return daoAuthenticationProvider;
+	}
 }
